a923092f4e3a81dd1eaeb5ace79f65e55b909fbc
Dan Chalmers <dan@danchalmers.me.uk>
2025-12-07 10:02:37 +0000

    refactor day 3 part 1 and do part 2

--- src/aoc/day3.py
+++ src/aoc/day3.py
@@ -1,24 +1,39 @@
 
 
 def part1_total_output_joltage(banks: list[str]) -> int:
-    js = [biggest_pair(bank) for bank in banks]
+    how_many = 2
+    js = [biggest_sequence(bank, how_many) for bank in banks]
     return sum(js)
 
 
-def biggest_pair(bank: str) -> int:
+def part2_total_output_joltage(banks: list[str]) -> int:
+    how_many = 12
+    js = [biggest_sequence(bank, how_many) for bank in banks]
+    return sum(js)
+
+
+def biggest_sequence(bank: str, how_many: int) -> int:
+    bank = [int(x) for x in bank]
+    bank_length = len(bank)
+    start_idx = 0
+    vals = []
+    for i in range(how_many):
+        up_to = bank_length - how_many + i + 1
+        val, idx = _max_from_to(bank, start_idx, up_to)
+        vals.append(val)
+        start_idx = idx + 1
+    return int("".join([str(v) for v in vals]))
+
+
+
+def _max_from_to(bank: list[int], start: int, end: int) -> tuple[int, int]:
     max_val = 0
     max_idx = 0
-    for idx, val in enumerate(bank[:-1]):
-        val = int(val)
+    for idx, val in enumerate(bank[start:end]):
         if val > max_val:
             max_val = val
             max_idx = idx
-    max_val2 = 0
-    for val2 in bank[max_idx + 1:]:
-        val2 = int(val2)
-        if val2 > max_val2:
-            max_val2 = val2
-    return int(f"{max_val}{max_val2}")
+    return max_val, start + max_idx
 
 
 if __name__ == "__main__":
@@ -26,3 +41,5 @@ if __name__ == "__main__":
         input_banks = f.read().splitlines()
         total_joltage = part1_total_output_joltage(input_banks)
         print(f"Part 1: {total_joltage}")
+        total_joltage = part2_total_output_joltage(input_banks)
+        print(f"Part 2: {total_joltage}")
--- test/test_day3.py
+++ test/test_day3.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc.day3 import biggest_pair, part1_total_output_joltage
+from aoc.day3 import biggest_sequence, part1_total_output_joltage, part2_total_output_joltage
 
 TEST_BANKS = """987654321111111
 811111111111119
@@ -8,23 +8,23 @@ TEST_BANKS = """987654321111111
 818181911112111"""
 
 
-def test_max_joltage_987654321111111():
-    result = biggest_pair("987654321111111")
+def test_part1_max_joltage_987654321111111():
+    result = biggest_sequence("987654321111111", 2)
     assert result == 98
 
 
-def test_max_joltage_811111111111119():
-    result = biggest_pair("811111111111119")
+def test_part1_max_joltage_811111111111119():
+    result = biggest_sequence("811111111111119", 2)
     assert result == 89
 
 
-def test_max_joltage_234234234234278():
-    result = biggest_pair("234234234234278")
+def test_part1_max_joltage_234234234234278():
+    result = biggest_sequence("234234234234278", 2)
     assert result == 78
 
 
-def test_max_joltage_818181911112111():
-    result = biggest_pair("818181911112111")
+def test_part1_max_joltage_818181911112111():
+    result = biggest_sequence("818181911112111", 2)
     assert result == 92
 
 
@@ -32,6 +32,32 @@ def test_part1_total_output_joltage():
     banks = TEST_BANKS.splitlines()
     result = part1_total_output_joltage(banks)
     assert result == 357
+
+
+def test_part2_max_joltage_987654321111111():
+    result = biggest_sequence("987654321111111", 12)
+    assert result == 987654321111
+
+
+def test_part2_max_joltage_811111111111119():
+    result = biggest_sequence("811111111111119", 12)
+    assert result == 811111111119
+
+
+def test_part2_max_joltage_234234234234278():
+    result = biggest_sequence("234234234234278", 12)
+    assert result == 434234234278
+
+
+def test_part2_max_joltage_818181911112111():
+    result = biggest_sequence("818181911112111", 12)
+    assert result == 888911112111
+
+
+def test_part2_total_output_joltage():
+    banks = TEST_BANKS.splitlines()
+    result = part2_total_output_joltage(banks)
+    assert result == 3121910778619
 
 
 
