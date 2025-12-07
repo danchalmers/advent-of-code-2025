6a4b54ea6284befc83f2855c13db8971a69f0b18
Dan Chalmers <dan@danchalmers.me.uk>
2025-12-07 10:51:36 +0000

    day 4 part 1 works

---
+++ src/aoc/day4.py
@@ -0,0 +1,49 @@
+
+
+type Row = int
+type Col = int
+type LayoutCells = list[list[str]]
+
+THRESHOLD = 4
+
+
+def part1_count_accessible(input: str) -> int:
+    layout, max_row, max_col = format_layout(input)
+    accessibles = [
+        is_accessible(layout, max_row, max_col, r, c)
+        for r in range(max_row + 1)
+        for c in range(max_col + 1)
+    ]
+    return sum(accessibles)
+
+
+def is_accessible(layout: LayoutCells, max_row: Row, max_col: Col, row: Row, col: Col) -> bool:
+    adjacents = adjacent(row, col, max_row, max_col)
+    value = layout[row][col]
+    if value != '@':
+        return False
+    adjacent_values = [layout[a[0]][a[1]] for a in adjacents]
+    return sum([a == '@' for a in adjacent_values]) < THRESHOLD
+
+
+def format_layout(layout: str) -> tuple[LayoutCells, Row, Col]:
+    layout = [[c for c in r] for r in layout.splitlines()]
+    max_row = len(layout) - 1
+    max_col = len(layout[0]) - 1
+    return layout, max_row, max_col
+
+
+def adjacent(row: Row, col: Col, max_row: Row, max_col: Col) -> list[tuple[Row, Col]]:
+    return [
+        (r, c)
+        for r in range(max(0, row-1), min(max_row+1, row+2))
+        for c in range(max(0, col-1), min(max_col+1, col+2))
+        if r != row or c != col
+    ]
+
+
+if __name__ == "__main__":
+    with open("data/day4.txt") as f:
+        layout = f.read()
+        count_accessible = part1_count_accessible(layout)
+        print(f"Part 1: {count_accessible}")
---
+++ test/test_day4.py
@@ -0,0 +1,118 @@
+import pytest
+
+from aoc.day4 import adjacent, format_layout, is_accessible, part1_count_accessible
+
+EXAMPLE = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
+
+
+def test_accessible_row0_col1():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 0, 1)
+    assert result == False
+
+
+def test_accessible_row0_col2():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 0, 2)
+    assert result == True
+
+
+def test_accessible_row0_col7():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 0, 7)
+    assert result == False
+
+
+def test_accessible_row1_col0():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 1, 0)
+    assert result == True
+
+
+def test_accessible_row1_col1():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 1, 1)
+    assert result == False
+
+
+def test_accessible_row2_col6():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 2, 6)
+    assert result == True
+
+def test_accessible_row9_col0():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 9, 0)
+    assert result == True
+
+
+def test_accessible_row9_col1():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 9, 1)
+    assert result == False
+
+
+def test_accessible_row9_col2():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 9, 2)
+    assert result == True
+
+
+def test_accessible_row9_col8():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 9, 8)
+    assert result == True
+
+
+def test_accessible_row9_col9():
+    layout, max_row, max_col = format_layout(EXAMPLE)
+    result = is_accessible(layout, max_row, max_col, 9, 9)
+    assert result == False
+
+
+def test_part1_count_accessible():
+    result = part1_count_accessible(EXAMPLE)
+    assert result == 13
+
+
+def test_adjacent_zero_corner():
+    result = adjacent(0, 0, 10, 10)
+    assert (0, 1) in result
+    assert (1, 0) in result
+    assert (1, 1) in result
+    assert (1, 2) not in result
+    assert (2, 1) not in result
+    assert (-1, -1) not in result
+
+
+def test_adjacent_end_corner():
+    result = adjacent(10, 10, 10, 10)
+    assert (9, 9) in result
+    assert (9, 10) in result
+    assert (10, 9) in result
+    assert (10, 10) not in result
+    assert (10, 11) not in result
+    assert (11, 10) not in result
+    assert (11, 11) not in result
+
+
+def test_adjacent_middle():
+    result = adjacent(3, 3, 10, 10)
+    assert (2, 2) in result
+    assert (2, 3) in result
+    assert (2, 4) in result
+    assert (3, 2) in result
+    assert (3, 4) in result
+    assert (4, 2) in result
+    assert (4, 3) in result
+    assert (4, 4) in result
+    assert (3, 3) not in result
